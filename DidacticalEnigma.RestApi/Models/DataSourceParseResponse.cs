93213998a59807279511830893f644cee4f977c0
milleniumbug <milleniumbug@protonmail.com>
2021-12-22 21:08:30 +0100

    Add Required attributes

--- DidacticalEnigma.RestApi/Models/DataSourceParseResponse.cs
+++ DidacticalEnigma.RestApi/Models/DataSourceParseResponse.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,12 @@ namespace DidacticalEnigma.RestApi.Models
 {
     public class DataSourceParseResponse
     {
+        [Required]
         public int Position { get; set; }
         
         public int? PositionEnd { get; set; }
         
+        [Required]
         public string DataSource { get; set; }
         
         public string Context { get; set; }
